a0e4d642a6cb6a4c41a03dbe6c9a964c928e6edb
marinpesa15 <pesamarin81@gmail.com>
2024-08-22 18:34:10 +0200

    Create new variable for player_turn_speed and player_speed in constants.py, create rotate, update and move method for the player in player.py, create update and draw groups in main.py and call draw and update for every object

--- main.py
+++ main.py
@@ -12,15 +12,22 @@ dt = 0
 
 def main():
   run = True
-
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+  updatable = pygame.sprite.Group()
+  drawable = pygame.sprite.Group()
+
+  updatable.add(player)
+  drawable.add(player)
 
   while run:
     dt = pygame.time.Clock().tick(60) / 1000
     win.fill("black")
-    player.draw(win)
+    for entity in drawable:
+      entity.draw(win)
+    for entity in updatable:
+      entity.update(dt)
     pygame.display.flip()
-    
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
