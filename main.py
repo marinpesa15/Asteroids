d1e9514902ce64a57c8b4352ffd00f942709ebaf
marinpesa15 <pesamarin81@gmail.com>
2024-08-22 16:37:53 +0200

    Set FPS in main loop, Create circleshape.py, Update constants.py, Create Player.py, Create Player object and draw player on screen

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 pygame.init()
 
 win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Asteroids")
+pygame.time.Clock()
+dt = 0
 
 
 def main():
   run = True
 
+  player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
   while run:
+    dt = pygame.time.Clock().tick(60) / 1000
     win.fill("black")
+    player.draw(win)
     pygame.display.flip()
+    
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
